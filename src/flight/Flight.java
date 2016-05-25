b8fea468efe594a1325845a326e44c7b4bbf1f96
Ignacio Cifuentes <ignacio.cifuentes948@gmail.com>
2016-05-25 11:38:48 -0300

    Implemented WeekDay enum and changes on Flight

--- src/flight/Flight.java
+++ src/flight/Flight.java
@@ -4,23 +4,57 @@ package flight;
 public class Flight {
 
     private Long flightTime;
-    private Long distance;
     private Long departureTime;
-    private WeekDay day;
+    private WeekDay departureDay;
     private Airport destination;
     private String airline;
     private Integer flightNumber;
 
 
-    public Flight(String airline, Integer flightNumber, ) {
+    public Flight(Long flightTime, Long departureTime, WeekDay departureDay,
+                  Airport destination, String airline, Integer flightNumber) {
+        this.flightTime = flightTime;
 
+        this.departureTime = departureTime;
+        this.departureDay = departureDay;
+        this.destination = destination;
+        this.airline = airline;
+        this.flightNumber = flightNumber;
     }
 
+    /**
+     * Concatenate airline and flightnumber to generate unique identifier
+     *
+     * @return string representation of flight
+     */
     public String getCode() {
         return "#" + this.airline + this.flightNumber;
     }
 
-    @Override
+    /**
+     * Get duration of this flight
+     *
+     * @return Long duration in minutes
+     */
+    public Long getFlightTime() { return flightTime; }
+
+    /**
+     * Get departure time in minutes
+     *
+     * @return
+     */
+    public Long getDepartureTime() { return departureTime; }
+
+    public WeekDay getDepartureDay() { return departureDay; }
+
+    public Airport getDestination() { return destination; }
+
+    /**
+     * Equality test
+     *
+     * @param o to be compared for equality
+     * @return boolean if o equals this
+     */
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
@@ -28,10 +62,13 @@ public class Flight {
         Flight flight = (Flight) o;
 
         return this.getCode().equals(flight.getCode());
-
     }
 
-    @Override
+    /**
+     * Unique identifier to each flight
+     *
+     * @return int hashcode of string unique to each flight
+     */
     public int hashCode() {
         return this.getCode().hashCode();
     }
---
+++ src/flight/WeekDay.java
@@ -0,0 +1,38 @@
+package flight;
+
+
+public enum WeekDay {
+    MONDAY(0), TUESDAY(1), WENDSDAY(2), THURSDAY(3), FRIDAY(4), SATURDAY(5), SUNDAY(6);
+
+    private int numDay;
+
+    public WeekDay getWeekDay(String string){
+        switch (string){
+            case "Lu":
+                return MONDAY;
+            case "Ma":
+                return TUESDAY;
+            case "Mi":
+                return WENDSDAY;
+            case "Ju":
+                return THURSDAY;
+            case "Vi":
+                return FRIDAY;
+            case "Sa":
+                return SATURDAY;
+            case "Do":
+                return SUNDAY;
+            default:
+                return null;
+        }
+    }
+
+    WeekDay(int numDay) {
+        this.numDay = numDay;
+    }
+
+    int distanceInDays(WeekDay other) {
+        return ( other.numDay - this.numDay );
+    }
+
+}
