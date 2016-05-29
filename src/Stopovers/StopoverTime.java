de21705cbb0044a246453970fe80dd924f049dbb
Martin Capparelli <mcapparelli@gmail.com>
2016-05-29 20:30:15 -0300

    Dijkstra priorities

--- src/Stopovers/StopoverTime.java
+++ src/Stopovers/StopoverTime.java
@@ -1,5 +1,20 @@
 package Stopovers;
 
-public class StopoverTime {
+public class StopoverTime extends Stopover {
 
+	public StopoverTime(Airport air, Flight fli){
+		super(air,fli);
+		criteriaWeight = fli.getFLightTime();
+	}
+	
+	public int compareTo(StopoverPrice other){
+		if(other == null)
+			throw new IllegalArgumentException();
+		if(criteriaWeight < other.getWeight())
+			return -1;
+		if(criteriaWeight > other.getWeight())
+			return 1;
+		return 0;
+		
+	}
 }
