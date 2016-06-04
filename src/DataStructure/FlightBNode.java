12feeff93008ac9ee0c42eec7b004c5b6cdc98b2
Martin Capparelli <mcapparelli@itba.edu.ar>
2016-06-04 17:02:11 -0300

    FlightBNode

--- src/DataStructure/FlightBNode.java
+++ src/DataStructure/FlightBNode.java
@@ -1,55 +1,64 @@
-//package DataStructure;
-//
-//import FlightAssistant.Flight;
-//
-//public class FlightBNode {
-//    private FlightBNode parent;
-//    private BTreeValue values;
-//    private FlightBNode leftChild;
-//    private FlightBNode middleChild;
-//    private FlightBNode rightChild;
-//    private int spacesFilled;
-//
-//    public FlightBNode(FlightBNode parent) {
-//        this.parent = parent;
-//        this.spacesFilled = 0;
-//    }
-//
-//    public boolean isNodeEmpty() {
-//        return leftValue == null && rightValue== null;
-//    }
-//
-//    public void addFlightToNode(Flight flight) {
-//       if(values.size > 2) {
-//           aux = values.getMiddleValue();
-//           if(this.parent == null) {
-//
-//           }
-//           this.parent.addFlightToNode(aux);
-//       }
-//    }
-//
-//    public FlightBNode getLeftChild() {
-//        return this.leftChild;
-//
-//    public FlightBNode getMiddleChild() {
-//        return this.middleChild;
-//    }
-//
-//    public FlightBNode getRightChild() {
-//        return this.rightChild;
-//    }
-//
-//    public void setLeftChild(FlightBNode leftChild) {
-//        this.leftChild = leftChild;
-//    }
-//
-//    public void setMiddleChild(FlightBNode middleChild) {
-//        this.middleChild = middleChild;
-//    }
-//
-//    public void setRightChild(FlightBNode rightChild) {
-//        this.rightChild = rightChild;
-//    }
-//
-//}
+package DataStructure;
+
+import FlightAssistant.Flight;
+
+public class FlightBNode {
+    private Flight left;
+    private Flight middle;
+    private Flight right;
+    private FlightBNode parent;
+    private FlightBNode leftChild;
+    private FlightBNode middleChild;
+    private FlightBNode rightChild;
+
+    public FlightBNode(FlightBNode parent, Flight leftValue, Flight rightValue){
+    	this.parent = parent;
+    	this.left = leftValue;
+    	this.right = rightValue;
+    }
+    
+    public boolean isLeaf(){
+    	if(leftChild == null && middleChild == null && rightChild == null)
+    		return true;
+    	return false;
+    }
+    
+    public void add(){
+    	
+    }
+    
+    public boolean isFull(){
+    	if(this.left != null && this.right != null && this.middle != null) {
+    		return true;
+    	}
+    	return false;
+    }
+    	
+    public Flight getLeft(){
+    	return this.left;
+    }
+    
+    public Flight getMiddle(){
+    	return this.middle;
+    }
+    
+    public Flight getRight(){
+    	return this.right;
+    }
+    
+    public BTreeNode split(FlightBNode node){
+    	FlightBNode current =  node;
+    	Flight leftAux;
+    	Flight middleAux;
+    	Flight rightAux;
+    	while(current.isFull()){
+    			leftAux = current.getLeft();
+    			rightAux = current.getRight();
+    			middleAux = current.getMiddle();
+    			current = current.parent;
+    			
+    	}
+    	
+    }
+    
+}
