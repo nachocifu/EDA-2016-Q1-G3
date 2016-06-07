6cd84ef1e78652a083db4edcf5bb8d58c10adaa1
Martin Capparelli <mcapparelli@itba.edu.ar>
2016-06-06 22:38:15 -0300

    Clase order para ordenar el tree

--- src/DataStructure/DataStructure.java
+++ src/DataStructure/DataStructure.java
@@ -6,7 +6,6 @@ import java.util.HashSet;
 import java.util.TreeMap;
 
 import FlightAssistant.Airport;
-import FlightAssistant.AviationGraph;
 import FlightAssistant.Flight;
 import FlightAssistant.WeekDay;
 
@@ -14,17 +13,43 @@ public class DataStructure {
 
 	private static int DAYS = 7;
 	
+	//La clase Order el unico fin que tiene es que el arbol se ordene acorde al tiempo de salida
+	//Si ponemos TreeMap<Double,Flight>no se admitirian 2 vuelos que salen a la misma hora.
+	private static class Order{
+		String str;
+		Double time;
+		
+		public Order(String str, Double time){
+			this.str = str;
+			this.time = time;
+		}
+		
+		public double getTime(){
+			return time;
+		}
+		
+		@Override
+		public boolean equals(Object obj) {
+			if(obj == null)
+				return false;
+			if(obj.getClass().equals(this.getClass()))
+				return false;
+			Order or = (Order) obj;
+			return or.str.equals(this.str);
+		}
+	}
+	
 	//Structures: TreeMap & HashMap<String,Airport> & TreeMap<String, Flight>
 	private DataStructure dataTree = new DataStructure();
-	private static TreeMap<String,Flight>[] flights = new TreeMap[DAYS];
+	private static TreeMap<Order,Flight>[] flights = new TreeMap[DAYS];
 	private static HashMap<String,Airport> airports = new HashMap();
 	
 	private DataStructure(){
 		for(int i = 0; i < DAYS; i++){
-			flights[i] = new TreeMap(new Comparator<Flight>(){
+			flights[i] = new TreeMap(new Comparator<Order>(){
 				@Override
-				public int compare(Flight f1, Flight f2) {
-					return (int) (f1.getDepartureTime() - f2.getDepartureTime());
+				public int compare(Order o1, Order o2) {
+					return (int) (o1.getTime() - o2.getTime());
 				}
 			});
 		}
@@ -34,7 +59,8 @@ public class DataStructure {
 	public static void addFlight(Flight fli, int day, String key){
 		if(fli == null || key == null)
 			throw new NullPointerException();
-		flights[day].put(key,fli);
+		Order or = new Order(fli.getCode(),fli.getDepartureTime());
+		flights[day].put(or,fli);
 		airports.get(key).addFlight(fli);
 	}
 	
@@ -53,7 +79,8 @@ public class DataStructure {
 		//como tambien lo que esta en el hash.
 		Flight flight = new Flight(flightTime, departureTime, WeekDay.getWeekDay(departureDay), destination, 
 								   origin, airline, flightNumber, price);
-		flights[WeekDay.getNumberOfDay(WeekDay.getWeekDay(departureDay).toString())].put(flight.getCode(), flight);
+		Order or = new Order(flight.getCode(),flight.getDepartureTime());
+		flights[WeekDay.getNumberOfDay(WeekDay.getWeekDay(departureDay).toString())].put(or, flight);
 		airports.get(origin.getCode()).addFlight(flight);
 	}
 	
@@ -64,8 +91,12 @@ public class DataStructure {
         airports.put(airport.getCode(), airport);
     }
 	
-	public Iterable<Airport> findAllAirports(){
+	public static Iterable<Airport> findAllAirports(){
        return airports.values();
+    }
+	
+	public static HashMap<String, Airport> getAirports(){
+        return airports;
     }
 	
 }	
